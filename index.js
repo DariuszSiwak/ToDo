08c2e3e5b57f0b8dc7bb3f4b050a0c589e64f653
Dariusz Siwak <darr.siww@gmail.com>
2022-01-19 10:45:51 +0100

    improved visual quality of the code

--- index.js
+++ index.js
@@ -3,33 +3,39 @@ const input = document.getElementById('title')
 
 submitButton.addEventListener('click', () => {
     const value = input.value
-        if(value === '') {
-        'return'
-    }
-    const element = document.createElement('div')
-    element.classList.add("list")
 
-    const title = document.createElement("p")
-    title.classList.add("title")
+    if(value === '') {
+        return
+    }
+
+    const element = document.createElement('div')
+    element.classList.add('list')
+
+    const title = document.createElement('p')
+    title.classList.add('title')
     element.appendChild(title)
     title.innerHTML = value
 
-    const image = document.createElement ("i")
-    image.classList.add("bin", "fab", "fa-bitbucket")
+    const image = document.createElement ('i')
+    image.classList.add('bin', 'fab', 'fa-bitbucket')
     element.appendChild(image)
     
     document.body.appendChild(element)
     
     title.addEventListener('click', () => {
+        
         if(title.style['textDecoration'] === 'line-through'){
             title.style['textDecoration'] = ''
-        }else{
+        } else {
             title.style['textDecoration'] = 'line-through'
         }
+        
     })
+
     image.addEventListener('click', () => {
         element.remove()
     })
+
     document.body.appendChild(element)
     input.value = ''
 })
